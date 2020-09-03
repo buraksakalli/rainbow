96a0a3a80271bf1c14113a9a6c05b9a62262f477
jinchung <jin.chung17@gmail.com>
2020-09-03 03:37:55 -0400

    Remove input and output reserve redux - incomplete

--- src/screens/ExchangeModal.js
+++ src/screens/ExchangeModal.js
@@ -87,7 +87,6 @@ export default function ExchangeModal({
     updateTxFee,
   } = useGas();
   const {
-    clearUniswapCurrenciesAndReserves,
     inputReserve,
     outputReserve,
   } = useUniswapCurrencyReserves();
@@ -232,12 +231,10 @@ export default function ExchangeModal({
     startPollingGasPrices();
     initWeb3Listener();
     return () => {
-      clearUniswapCurrenciesAndReserves();
       stopPollingGasPrices();
       stopWeb3Listener();
     };
   }, [
-    clearUniswapCurrenciesAndReserves,
     initWeb3Listener,
     isDeposit,
     isWithdrawal,
